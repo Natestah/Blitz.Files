0629a1d2775bc7de3b07334831746e6f20aa66d8
Natestah <natestah@gmail.com>
2024-10-17 11:51:39 -0700

    Blitz.Files Initial Public Repo Submit
    Blitz Files, uses internal dictionary and file watchers to keep track of files, since enumerating files can take quite a bit of time in itself.

---
+++ BlitzFilesConsoleDemo/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Diagnostics;
+using Blitz.Files;
+using Blitz.Interfacing;
+
+string? directory = string.Empty;
+while (true)
+{
+    Console.WriteLine("Input a valid directory to run Blitz Files Demo on:");
+    directory = Console.ReadLine();
+    if (string.IsNullOrEmpty(directory))
+    {
+        continue;
+    }
+    if (Directory.Exists(directory))
+    {
+        break;
+    }
+}
+
+var timer = Stopwatch.StartNew();
+var files = new List<string>();
+Console.WriteLine("Enumerating files with 'Directory.Enumeratefiles");
+files = Directory.EnumerateFiles(directory, "*.*", SearchOption.AllDirectories).ToList();
+timer.Stop();
+Console.WriteLine($"Elapsed time: {timer.ElapsedMilliseconds} MS");
+Console.WriteLine($"Total files: {files.Count}");
+
+
+Console.WriteLine("Cold Enumeration of files using FileDiscoverer");
+timer = Stopwatch.StartNew();
+
+var searchPath = new SearchPath{ Folder = directory };
+var paths = new List<SearchPath>{searchPath};
+var discoverer = new FileDiscovery(paths, useGitIgnore:false);
+discoverer.WaitUntilFinished(new CancellationTokenSource());
+files = [..discoverer.EnumerateAllFiles(new CancellationTokenSource())];
+timer.Stop();
+
+Console.WriteLine($"Elapsed time: {timer.ElapsedMilliseconds} MS");
+Console.WriteLine($"Total files: {files.Count}");
+
+Console.WriteLine("Cached Enumeration of files using FileDiscoverer");
+timer = Stopwatch.StartNew();
+files = [..discoverer.EnumerateAllFiles(new CancellationTokenSource())];
+timer.Stop();
+Console.WriteLine($"Elapsed time: {timer.ElapsedMilliseconds} MS");
+Console.WriteLine($"Total files: {files.Count}");
+
+
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Blitz.Files
+
 C# Parallel Persistent File Discovery for Blitz Search
+
+This helps serve slow enumeration of files
